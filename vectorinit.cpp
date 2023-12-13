10f6099a32d228b6c49f3828b91a908070f4253a
munir79 <jakirhossainmunir79@gmail.com>
2023-12-13 21:06:27 +0600

    copy element in vector

--- vectorinit.cpp
+++ vectorinit.cpp
@@ -1,13 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  //  vector<int> v; // type 1
-  vector <int>v(5);
-  vector <int> v1(7,10);
-  for(int i=0;i<v1.size();i++){
-    cout<< v1[i]<< " ";
+  vector <int> v; // type 1
+  cout<< v.size();
+  cout << endl;
+  vector <int> v1(5); // type 2
+  cout << v1.size();
+  cout << endl;
+  vector <int> v2(4,10); //type 3
+  for(int i=0;i<v2.size();i++){
+    cout << v2[i] << " ";
   }
-  cout<< endl;
-  
-    cout<< v.size();
+
+  cout << endl;
+  vector<int> v3(3,50);
+
+  // copy vector
+
+  vector<int>v4(v3);
+  for(int i=0;i<v4.size();i++){
+    cout << v4[i]<< " ";
+  }
+
+
+
 }
