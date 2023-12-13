ec1f17b5927cc5e726c5762a4f728808bc578b42
munir79 <jakirhossainmunir79@gmail.com>
2023-12-13 22:10:25 +0600

    capacit

--- 2.vectorCapacity.cpp
+++ 2.vectorCapacity.cpp
@@ -4,5 +4,10 @@ int main(){
     vector <int> v;
     cout <<v.capacity()<<endl;
     v.push_back(10);
+    cout <<v.capacity()<<endl;
+    v.push_back(30);
+    cout<<v.capacity()<<endl;
+    v.push_back(78);
     cout <<v.capacity();
+    // remeber capacity increase 2 multiplies
 }
